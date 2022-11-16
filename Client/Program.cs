2f8d7e830b89f257b391fb6c49cd48086bfc7d1d
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2022-11-16 02:59:24 +0000

    Add blazorstate to client

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,6 +8,24 @@ namespace Pwa.Client;
 
 public class Program
 {
+  public static void ConfigureServices(IServiceCollection aServiceCollection, IConfiguration aConfiguration)
+  {
+    aServiceCollection.AddBlazorState
+    (
+      (aOptions) =>
+      {
+    #if ReduxDevToolsEnabled
+            aOptions.UseReduxDevTools( options => options.Trace = true);
+    #endif
+        aOptions.Assemblies =
+          new Assembly[]
+          {
+                  typeof(Program).GetTypeInfo().Assembly,
+          };
+      }
+    );
+  }
+
   public static async Task Main(string[] args)
   {
     var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -15,8 +33,9 @@ public class Program
     builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
-    builder.Services.AddBlazorState(options => options.Assemblies = new Assembly[] { typeof(Program).GetTypeInfo().Assembly });
     builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+    ConfigureServices(builder.Services, builder.Configuration);
+    
 
     await builder.Build().RunAsync();
   }
